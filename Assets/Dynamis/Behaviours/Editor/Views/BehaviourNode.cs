eec3879093fa200b4be387d08a77cbfe4969df22
Huawei Li <MajyoArisu@outlook.com>
2025-07-16 00:30:49 +0800

    Replacing style based node position with transform based

--- Assets/Dynamis/Behaviours/Editor/Views/BehaviourNode.cs
+++ Assets/Dynamis/Behaviours/Editor/Views/BehaviourNode.cs
@@ -74,14 +74,25 @@ namespace Dynamis.Behaviours.Editor.Views
             get => _startNodePosition; 
             set => _startNodePosition = value; 
         }
+        
+        // public Vector2 CanvasPosition
+        // {
+        //     get => new(style.left.value.value, style.top.value.value);
+        //     set
+        //     {
+        //         style.left = value.x;
+        //         style.top = value.y;
+        //     }
+        // }
 
+        // 基于transform的节点位置计算，用于替代基于style的计算，尚不确定是否会引入布局问题
         public Vector2 CanvasPosition
         {
-            get => new(style.left.value.value, style.top.value.value);
+            get => new(transform.position.x, transform.position.y);
             set
             {
-                style.left = value.x;
-                style.top = value.y;
+                transform.position = new Vector3(value.x, value.y, 0);
+                onPositionChanged?.Invoke();
             }
         }
 
