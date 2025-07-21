9452b66fe3d9eaa3630deaac31e2297713a10db9
Huawei Li <MajyoArisu@outlook.com>
2025-07-21 16:53:57 +0800

    Refactor BehaviourTree

--- Assets/Dynamis/Behaviours/Runtimes/BehaviourTree.cs
+++ Assets/Dynamis/Behaviours/Runtimes/BehaviourTree.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Dynamis.Behaviours.Runtimes
 {
+    public enum TraversalOrder
+    {
+        Bfs,  // 广度优先搜索
+        Dfs   // 深度优先搜索
+    }
+
     [CreateAssetMenu(fileName = "New Behaviour Tree", menuName = "Dynamis/Behaviour Tree")]
     public class BehaviourTree : ScriptableObject
     {
         [SerializeField] private Node rootNode;
-        [SerializeField] private List<Node> nodes = new List<Node>();
+        [SerializeField] private List<Node> nodes = new();
         [SerializeField] private NodeState treeState = NodeState.Running;
         
         public Node RootNode 
@@ -48,35 +55,115 @@ namespace Dynamis.Behaviours.Runtimes
                 rootNode.Reset();
         }
 
-        public BehaviourTree Clone()
+        public void Traverse(Action<Node> action, TraversalOrder order = TraversalOrder.Bfs)
         {
-            BehaviourTree tree = Instantiate(this);
-            tree.rootNode = rootNode?.Clone();
-            tree.nodes = new List<Node>();
-            
-            if (tree.rootNode != null)
-                tree.CollectNodes(tree.rootNode);
-                
-            return tree;
+            if (rootNode == null || action == null)
+                return;
+
+            if (order == TraversalOrder.Bfs)
+            {
+                TraverseBfs(action);
+            }
+            else
+            {
+                TraverseDfs(action);
+            }
         }
 
-        private void CollectNodes(Node node)
+        private void TraverseBfs(Action<Node> action)
         {
-            if (node != null)
+            var queue = PoolUtils.GetQueue();
+            var children = PoolUtils.GetList();
+            try
             {
-                nodes.Add(node);
-                node.SetBehaviourTree(this);
-                
-                if (node is CompositeNode composite)
+                queue.Enqueue(rootNode);
+
+                while (queue.Count > 0)
                 {
-                    foreach (var child in composite.Children)
-                        CollectNodes(child);
-                }
-                else if (node is DecoratorNode decorator && decorator.Child != null)
-                {
-                    CollectNodes(decorator.Child);
+                    var currentNode = queue.Dequeue();
+                    action(currentNode);
+
+                    // 获取子节点并加入队列
+                    GetChildren(currentNode, children);
+                    
+                    foreach (var child in children)
+                    {
+                        if (child != null)
+                        {
+                            queue.Enqueue(child);
+                        }
+                    }
+                    
+                    children.Clear();
                 }
             }
+            finally
+            {
+                PoolUtils.ReleaseQueue(queue);
+            }
+        }
+
+        private void TraverseDfs(Action<Node> action)
+        {
+            var stack = PoolUtils.GetStack();
+            var children = PoolUtils.GetList();
+            try
+            {
+                stack.Push(rootNode);
+
+                while (stack.Count > 0)
+                {
+                    var currentNode = stack.Pop();
+                    action(currentNode);
+
+                    // 获取子节点并以逆序加入栈（这样遍历时会按正序访问）
+                    GetChildren(currentNode, children);
+                    
+                    for (int i = children.Count - 1; i >= 0; i--)
+                    {
+                        if (children[i] != null)
+                            stack.Push(children[i]);
+                    }
+                    
+                    children.Clear();
+                }
+            }
+            finally
+            {
+                PoolUtils.ReleaseStack(stack);
+                PoolUtils.ReleaseList(children);
+            }
+        }
+
+        private static void GetChildren(Node node, List<Node> children)
+        {
+            switch (node)
+            {
+                case CompositeNode composite:
+                    children.AddRange(composite.Children);
+                    break;
+                case DecoratorNode decorator when decorator.Child != null:
+                    children.Add(decorator.Child);
+                    break;
+            }
+        }
+
+        public BehaviourTree Clone()
+        {
+            var tree = Instantiate(this);
+            tree.rootNode = rootNode?.Clone();
+            tree.nodes = new List<Node>();
+
+            if (tree.rootNode != null)
+            {
+                tree.Traverse(node =>
+                {
+                    tree.nodes.Add(node);
+                    node.SetBehaviourTree(tree);
+                });
+            }
+                
+            return tree;
         }
     }
 }
---
+++ Assets/Dynamis/Behaviours/Runtimes/PoolUtils.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Pool;
+
+namespace Dynamis.Behaviours.Runtimes
+{
+    /// <summary>
+    /// 提供Queue和Stack的对象池化工具类
+    /// </summary>
+    public static class PoolUtils
+    {
+        // Queue&lt;Node&gt;的对象池
+        private static readonly ObjectPool<Queue<Node>> QueuePool = new(
+            createFunc: () => new Queue<Node>(),
+            actionOnGet: queue => queue.Clear(),
+            actionOnRelease: queue => queue.Clear(),
+            actionOnDestroy: null,
+            collectionCheck: true,
+            defaultCapacity: 10,
+            maxSize: 100
+        );
+
+        // Stack&lt;Node&gt;的对象池
+        private static readonly ObjectPool<Stack<Node>> StackPool = new(
+            createFunc: () => new Stack<Node>(),
+            actionOnGet: stack => stack.Clear(),
+            actionOnRelease: stack => stack.Clear(),
+            actionOnDestroy: null,
+            collectionCheck: true,
+            defaultCapacity: 10,
+            maxSize: 100
+        );
+
+        // List&lt;Node&gt;的对象池
+        private static readonly ObjectPool<List<Node>> ListPool = new(
+            createFunc: () => new List<Node>(),
+            actionOnGet: list => list.Clear(),
+            actionOnRelease: list => list.Clear(),
+            actionOnDestroy: null,
+            collectionCheck: true,
+            defaultCapacity: 10,
+            maxSize: 100
+        );
+
+        /// <summary>
+        /// 从池中获取一个Queue&lt;Node&gt;
+        /// </summary>
+        /// <returns>清空的Queue&lt;Node&gt;实例</returns>
+        public static Queue<Node> GetQueue()
+        {
+            return QueuePool.Get();
+        }
+
+        /// <summary>
+        /// 将Queue&lt;Node&gt;归还到池中
+        /// </summary>
+        /// <param name="queue">要归还的队列</param>
+        public static void ReleaseQueue(Queue<Node> queue)
+        {
+            if (queue != null)
+            {
+                QueuePool.Release(queue);
+            }
+        }
+
+        /// <summary>
+        /// 从池中获取一个Stack&lt;Node&gt;
+        /// </summary>
+        /// <returns>清空的Stack&lt;Node&gt;实例</returns>
+        public static Stack<Node> GetStack()
+        {
+            return StackPool.Get();
+        }
+
+        /// <summary>
+        /// 将Stack&lt;Node&gt;归还到池中
+        /// </summary>
+        /// <param name="stack">要归还的栈</param>
+        public static void ReleaseStack(Stack<Node> stack)
+        {
+            if (stack != null)
+            {
+                StackPool.Release(stack);
+            }
+        }
+
+        /// <summary>
+        /// 从池中获取一个List&lt;Node&gt;
+        /// </summary>
+        /// <returns>清空的List&lt;Node&gt;实例</returns>
+        public static List<Node> GetList()
+        {
+            return ListPool.Get();
+        }
+
+        /// <summary>
+        /// 将List&lt;Node&gt;归还到池中
+        /// </summary>
+        /// <param name="list">要归还的列表</param>
+        public static void ReleaseList(List<Node> list)
+        {
+            if (list != null)
+            {
+                ListPool.Release(list);
+            }
+        }
+    }
+}
